3e7ed7a70144c5349f61742f63fbebcc2d02f1ee
Haoran Zu <134699083+Rachel19940227@users.noreply.github.com>
2024-04-13 15:56:19 -0400

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,11 @@
+
+public class Program {
+	public static void main(String[] args) {
+		String name = "Student";
+		System.out.println("Welcome to CST8116!");
+		System.out.print("Java says: Hello ");
+		System.out.print(name);
+		System.out.println(); // new-line
+		System.out.println("Program modified by " + name);
+	}
+}
